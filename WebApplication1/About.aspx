c058e573d6d5d4a61cd17fa2e3a396d4c7ef4564
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2019-10-22 17:18:49 -0300

    first commit

---
+++ WebApplication1/About.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="WebApplication1.About" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" Width="407px" />
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <br />
+</asp:Content>
---
+++ WebApplication1/About.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace WebApplication1
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String cadenaConexion = "Data source=CRISTIAN; initial catalog=servicio;integrated security=true";
+                SqlConnection conexionBD = new SqlConnection(cadenaConexion);
+                conexionBD.Open();
+                Label1.Text = "conexion exitosa";
+            }
+            catch (Exception ex)
+            {
+               Label1.Text = ex.Message;
+            }
+        }
+    }
+}
